120d337e9c08ea400e9b90fc157dcb772b6aeb15
ElDucche <gduc.37@gmail.com>
2025-03-16 01:29:35 +0100

    Réajout de la dépendance spring-boot-starter-data-jpa dans le fichier pom.xml

--- src/main/java/elducche/projet_3_spring/controller/MessageController.java
+++ src/main/java/elducche/projet_3_spring/controller/MessageController.java
@@ -5,7 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 import elducche.projet_3_spring.dto.MessageDTO;
-import elducche.projet_3_spring.repository.MessageRepository;
+
 import elducche.projet_3_spring.services.MessageService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
