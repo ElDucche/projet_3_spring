ec5930d3956167bfaa42cdd25042a2eb22ea693b
ElDucche <gduc.37@gmail.com>
2025-04-13 23:46:40 +0200

    Ajout de la validation des données dans RegisterRequest avec des annotations pour l'email, le nom et le mot de passe, incluant des messages d'erreur personnalisés.

--- src/main/java/elducche/projet_3_spring/dto/RegisterRequest.java
+++ src/main/java/elducche/projet_3_spring/dto/RegisterRequest.java
@@ -1,14 +1,21 @@
 package elducche.projet_3_spring.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class RegisterRequest {
     @NotNull(message = "Email is required")
+    @Email(message = "Invalid email format")
     private String email;
+
     @NotNull(message = "Name is required")
+    @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters")
     private String name;
+
     @NotNull(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters")
     private String password;
 }
